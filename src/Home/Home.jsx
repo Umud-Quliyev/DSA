34a7df7681cb88f50ed17c098b6faa602981d7f2
Telman Mammadov <tehsilelm20212@gmail.com>
2025-02-05 16:59:57 +0400

    accordion in FAQ

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,14 +2,16 @@ import TrainingProgram from "../components/Training_Program/page";
 import Customers from "../components/Customers/Customers";
 import Team from "../components/Team/Team";
 import MainSection from "../components/MainSection.jsx/MainSection";
+import FAQ from "../components/FAQ/page";
 
 const Home = () => {
   return (
     <div>
-      <MainSection/>
+      <MainSection />
       <Customers />
       <TrainingProgram />
       <Team />
+      <FAQ />
     </div>
   );
 };
