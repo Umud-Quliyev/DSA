27c15c5a1b28e4d4e5e207cf6b0c2abc68618005
Toghrul <adadsa>
2025-02-05 14:16:46 +0400

    new update about tailwind

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,22 @@
-import logo from "../../assets/svg/8bf0d7d8.svg"
+import logo from "../../assets/svg/8bf0d7d8.svg";
 const Header = () => {
   return (
-    <div >
-<img src={logo} alt="" />
-<ul className="">
-  <li>Əsas</li>
-  <li>Müştərilər</li>
-  <li>Təlim Proqramı</li>
-  <li>Komandamız</li>
-  <li>FAQ</li>
-  <li>Əlaqə</li>
-  <li>Təlimlər</li>
-</ul>
-    </div>
-  )
-}
+    <div className="px-0 flex bg-[red] w-full">
+      <div className="w-48">
 
-export default Header
+        <img src={logo} alt="" className="w-full"/>
+      </div>
+      <ul className="w-full flex justify-evenly">
+        <li>Əsas</li>
+        <li>Müştərilər</li>
+        <li>Təlim Proqramı</li>
+        <li>Komandamız</li>
+        <li>FAQ</li>
+        <li>Əlaqə</li>
+        <li>Təlimlər</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
