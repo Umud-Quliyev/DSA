9df7ce688d2fe1f13b583feb8a0e058463183892
Toghrul <adadsa>
2025-02-05 12:51:10 +0400

    header section added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-
+import logo from "../../assets/svg/8bf0d7d8.svg"
 const Header = () => {
   return (
-    <div>Header</div>
+    <div >
+<img src={logo} alt="" />
+<ul className="">
+  <li>Əsas</li>
+  <li>Müştərilər</li>
+  <li>Təlim Proqramı</li>
+  <li>Komandamız</li>
+  <li>FAQ</li>
+  <li>Əlaqə</li>
+  <li>Təlimlər</li>
+</ul>
+    </div>
   )
 }
 
