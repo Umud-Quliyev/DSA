2c64c4ee8f8f619eb603f5a3350e15d5c475921e
Umud <umudquliyev.1806@gmail.com>
2025-02-05 12:51:03 +0400

    style default

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Home from "./Home/Home";
 import Practics from "./Practics/Practics";
+import "./App.css"
 
 function App() {
   return (
