bb6dfc0a835a403e92827b15d06f4f9fc7713937
Umud <umudquliyev.1806@gmail.com>
2025-02-06 16:42:37 +0400

    fix code

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,10 +1,10 @@
-import TrainingProgram from "../components/Training_Program/page";
 import Customers from "../components/Customers/Customers";
 import Team from "../components/Team/Team";
 import MainSection from "../components/MainSection.jsx/MainSection";
 import FAQ from "../components/FAQ/page";
 import Contact from "../components/Contact/Contact";
 import ScrollToTop from "../components/ScrollToTop/ScrollToTop";
+import TrainingProgram from "../components/Training_Program/Page";
 
 const Home = () => {
   return (
