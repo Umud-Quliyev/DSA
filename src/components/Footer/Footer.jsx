9ca4ffe7895c5896f94607d859bed26610cd6181
Telman Mammadov <tehsilelm20212@gmail.com>
2025-02-06 17:42:17 +0400

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import {
+  Facebook,
+  Twitter,
+  LinkedIn,
+  Instagram,
+  GitHub,
+  YouTube,
+} from "@mui/icons-material";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="bg-gray-900 text-white py-6 px-4 mt-10">
+      <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center text-center md:text-left">
+        {/* Logo & Copyright */}
+        <div className="mb-4 md:mb-0">
+          <h2 className="text-xl font-semibold">Data Science Academy</h2>
+          <p className="text-sm text-gray-400">
+            &copy; {new Date().getFullYear()} All rights reserved.
+          </p>
+        </div>
 
-export default Footer
+        {/* Social Icons */}
+        <div className="flex space-x-4">
+          <a
+            target="_blank"
+            href="https://www.facebook.com/datascienceacademy.az/"
+            className="hover:text-blue-500 transition"
+          >
+            <Facebook fontSize="large" />
+          </a>
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/company/dsa-azerbaijan"
+            className="hover:text-blue-700 transition"
+          >
+            <LinkedIn fontSize="large" />
+          </a>
+          <a
+            target="_blank"
+            href="https://www.instagram.com/datascienceacademy_/"
+            className="hover:text-pink-500 transition"
+          >
+            <Instagram fontSize="large" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
