2e5f44d6fe0ed1047a37108f431ea60c4f743c96
Toghrul <adadsa>
2025-02-06 15:10:07 +0400

    navbar animation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,31 +1,155 @@
 import { Link } from "react-router-dom";
 import logo from "../../assets/svg/8bf0d7d8.svg";
+import logoBlack from "../../assets/svg/logoBlack.svg";
+import { IoMdArrowDropdown } from "react-icons/io";
+import { useState, useEffect } from "react";
+
 const Header = () => {
+  const [dropdown, setDropdown] = useState(false);
+  const [color, setColor] = useState(false);
+  const [chooseScroll, setChooseScroll] = useState(false);
+  const [currentScroll, setCurrentScroll] = useState(0);
+  /* useEffect(() => {
+    const handleScroll = () => {
+      const nav = document.querySelector(".nav");
+      const scroll = window.scrollY;
+      if (scroll > 100) {
+        if (scroll > currentScroll) {
+          nav.classList.add("scrolled");
+          setColor(true);
+          setChooseScroll(false);
+        } else {
+          setChooseScroll(true);
+        }
+      }
+      setCurrentScroll(scroll);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []); */
+
+  useEffect(() => {
+    const handleNavigation = () => {
+      const scroll = window.scrollY;
+
+      if (scroll > 100) {
+        setColor(true); // Logo rengini değiştirmek için
+        setChooseScroll(true); // Navbar scroll olduğunda sabit olsun
+      } else {
+        setColor(false);
+        setChooseScroll(false); // 100'ün altına inince navbar normal konumuna gelsin
+      }
+
+      setCurrentScroll(scroll); // Geçerli scroll değerini güncelle
+    };
+
+    window.addEventListener("scroll", handleNavigation); // Scroll eventini dinle
+    return () => {
+      window.removeEventListener("scroll", handleNavigation); // component unmount olduğunda event dinleyiciyi kaldır
+    };
+  }, [currentScroll]); // currentScroll değiştikçe useEffect çalışacak
+
+  const handleClick = () => {
+    setDropdown(!dropdown);
+  };
   return (
-    <div className="flex justify-center items-center h-30  text-[#fff]">
-      <div className="w-48">
-        <img src={logo} alt="" className="text-[]" />
+    <div
+      className={`flex justify-center items-center h-20  text-[#fff] nav z-10 ${
+        chooseScroll ? "scrolled" : "scrollUp"
+      }`}
+    >
+      <div className="w-40">
+        {color ? (
+          <img src={logoBlack} alt="" />
+        ) : (
+          <img src={logo} alt="" className="" />
+        )}
       </div>
       <ul className="w-3/5 flex items-center justify-evenly ">
         <Link to="">
           {" "}
-          <li>Əsas</li>
+          <li className="hover:text-[#fccd00]">Əsas</li>
         </Link>
         <Link to="">
-          <li>Təlim Proqramı</li>
+          <li className="hover:text-[#fccd00]">Təlim Proqramı</li>
         </Link>
         <Link to="">
-          <li>Komandamız</li>
+          <li className="hover:text-[#fccd00]">Komandamız</li>
         </Link>
         <Link to="">
           {" "}
-          <li>FAQ</li>
+          <li className="hover:text-[#fccd00]">FAQ</li>
         </Link>
         <Link to="">
-          <li>Əlaqə</li>
+          <li className="hover:text-[#fccd00]">Əlaqə</li>
         </Link>
         <Link to="">
-          <li>Təlimlər</li>
+          <li
+            className="flex items-center gap-1 relative hover:text-[#fccd00]"
+            onClick={handleClick}
+          >
+            Təlimlər <IoMdArrowDropdown />
+            {dropdown && (
+              <div className="absolute top-10 right-0 w-max bg-[#FFF] px-4 py-5  flex gap-10 flex-wrap rounded-[5px]">
+                <div>
+                  <h1 className="text-[#2fa8a5] font-bold">Data Analitika</h1>
+                  <p className="text-[#50264E] font-bold">
+                    Beginner to Intermediate
+                  </p>
+                  <div className="flex flex-col">
+                    <Link className="text-[#50264E] pr-3 transition  duration-300 ease hover:text-[#fccd00]  hover:bg-[#f8f9fb]">
+                      - Excel ilə Data Analitikası
+                    </Link>
+                    <Link className="text-[#50264E] pr-3 transition  duration-300 ease hover:text-[#fccd00] hover:bg-[#f8f9fb]">
+                      - Power BI ilə Data Analitikası
+                    </Link>
+                    <Link className="text-[#50264E] pr-3 transition  duration-300 ease hover:text-[#fccd00] hover:bg-[#f8f9fb]">
+                      - SQL ilə Data Analitikası
+                    </Link>
+                    <Link className="text-[#50264E] pr-3 transition  duration-300 ease hover:text-[#fccd00] hover:bg-[#f8f9fb]">
+                      - Tableau ilə Data Analitikası
+                    </Link>
+                    <Link className="text-[#50264E] pr-3 transition  duration-300 ease hover:text-[#fccd00] hover:bg-[#f8f9fb]">
+                      - SPSS ilə Statistika və Data Analitikası
+                    </Link>
+                    <Link className="text-[#50264E] pr-3 transition  duration-300 ease hover:text-[#fccd00] hover:bg-[#f8f9fb]">
+                      - R ilə Data Analitikası
+                    </Link>
+                  </div>
+                </div>
+                <div>
+                  <h1 className="text-[#2fa8a5] font-bold">Data Science</h1>
+                  <p className="text-[#50264E] font-bold">
+                    Beginner to Intermediate
+                  </p>
+                  <div className="flex flex-col">
+                    <Link className="text-[#50264E] pr-3 transition  duration-300 ease hover:text-[#fccd00] hover:bg-[#f8f9fb]">
+                      - Python ilə Data Analitikası
+                    </Link>
+                    <Link className="text-[#50264E] pr-3 transition  duration-300 ease hover:text-[#fccd00] hover:bg-[#f8f9fb]">
+                      - Big Data
+                    </Link>
+                    <Link className="text-[#50264E] pr-3 transition  duration-300 ease hover:text-[#fccd00] hover:bg-[#f8f9fb]">
+                      - Python ilə Dərin Öyrənmə
+                    </Link>
+                    <Link className="text-[#50264E] pr-3 transition  duration-300 ease hover:text-[#fccd00] hover:bg-[#f8f9fb]">
+                      - SPSS ilə Data Hazırlanması və Maşın Öyrənməsi
+                    </Link>
+                    <Link className="text-[#50264E] pr-3 transition  duration-300 ease hover:text-[#fccd00] hover:bg-[#f8f9fb]">
+                      - R ilə Maşın Öyrənməsi
+                    </Link>
+                    <Link className="text-[#50264E] pr-3 transition  duration-300 ease hover:text-[#fccd00] hover:bg-[#f8f9fb]">
+                      - Python ilə Maşın Öyrənməsi
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            )}
+          </li>
         </Link>
       </ul>
     </div>
