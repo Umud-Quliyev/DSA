e798764898712af45dc6a7d88055fbac8da61cfd
Toghrul <adadsa>
2025-02-06 22:57:01 +0400

    text size updated

--- src/components/MainSection.jsx/MainSection.jsx
+++ src/components/MainSection.jsx/MainSection.jsx
@@ -4,17 +4,17 @@ const MainSection = () => {
   return (
     <section id="main" name="main" className="bg-[url(./assets/png/main-bg.jpg)] h-[100vh] w-full">
       <Header />
-      <div className="text-[#fff] flex  flex-col items-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
-          <h1 className="text-[40px]">12 HƏFTƏDƏ</h1>
-          <h2 className="text-[60px] font-extrabold">Data Scientist Olmaq?</h2>
+      <div className="text-[#fff] flex  flex-col items-center absolute top-2/3 left-1/2 transform -translate-x-1/2 -translate-y-2/3 ">
+          <h1 className="text-[60px] font-[200]">12 Həftədə</h1>
+          <h2 className="text-[60px]  font-bold">Data Scientist Olmaq?</h2>
         <div className="bg-[#310d31] rounded-[10px] flex flex-col items-center  p-4">
-          <span className="text-[#fecb00] text-[20px]">
+          <span className="text-[#ffca05] text-[20px]">
             Data Science Academy
           </span>
-          <h3 className="text-[#fecb00] text-[50px] font-bold ">Bootcamp</h3>
+          <h3 className="text-[#ffca05] text-[40px] font-extrabold ">BOOTCAMP</h3>
           <span className="text-[30px] font-bold">1 Fevral 2025</span>
         </div>
-        <span className="text-center text-[20px]">
+        <span className="text-center text-[18px]">
           En müasir tədris proqramı, <br /> real biznes layihələri və fərdi{" "}
           <br /> karyera dəstəyi əsasında <br /> bizimlə mümkündür!
         </span>
