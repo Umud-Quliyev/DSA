6c2d7c327400655ef69598f3a304377c2310b933
Umud <umudquliyev.1806@gmail.com>
2025-02-10 14:42:38 +0400

    recode faq

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,9 +1,8 @@
 import Customers from "../components/Customers/Customers";
 import Team from "../components/Team/Team";
 import MainSection from "../components/MainSection.jsx/MainSection";
-import FAQ from "../components/FAQ/page";
+import FAQ from "../components/FAQ/Faq";
 import Contact from "../components/Contact/Contact";
-import ScrollToTop from "../components/ScrollToTop/ScrollToTop";
 import TrainingProgram from "../components/Training_Program/TrainingProgram";
 
 const Home = () => {
---
+++ src/components/FAQ/Faq.jsx
@@ -0,0 +1,104 @@
+import React, { useEffect, useState } from "react";
+import "./Faq.css";
+import { styled } from "@mui/material/styles";
+import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
+import MuiAccordion from "@mui/material/Accordion";
+import MuiAccordionSummary, {
+  accordionSummaryClasses,
+} from "@mui/material/AccordionSummary";
+import MuiAccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+
+const Accordion = styled((props) => (
+  <MuiAccordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+  border: `1px solid ${theme.palette.divider}`,
+  "&:not(:last-child)": {
+    borderBottom: 0,
+  },
+  "&::before": {
+    display: "none",
+  },
+}));
+
+const AccordionSummary = styled((props) => (
+  <MuiAccordionSummary
+    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: "0.9rem" }} />}
+    {...props}
+  />
+))(({ theme }) => ({
+  backgroundColor: "rgba(0, 0, 0, .03)",
+  flexDirection: "row-reverse",
+  [`& .${accordionSummaryClasses.expandIconWrapper}.${accordionSummaryClasses.expanded}`]:
+    {
+      transform: "rotate(90deg)",
+    },
+  [`& .${accordionSummaryClasses.content}`]: {
+    marginLeft: theme.spacing(1),
+  },
+}));
+
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+  padding: theme.spacing(2),
+  borderTop: "1px solid rgba(0, 0, 0, .125)",
+}));
+
+const Faq = () => {
+  const [expanded, setExpanded] = useState("false");
+
+  const handleChange = (panel) => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false);
+  };
+
+  const [faqItems, setFaqItems] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const faqRes = await fetch("http://localhost:3000/faq");
+        const faqData = await faqRes.json();
+        setFaqItems(faqData);
+      } catch (error) {
+        console.error("Veri çekme hatası:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="faq">
+      <div className="contanierr">
+        <div className="faq__title">
+          <h2>FAQ</h2>
+        </div>
+        <div className="faq__list">
+          {faqItems.length > 0 ? (
+            faqItems.map((item, index) => (
+              <Accordion
+                key={index}
+                className="faq__item"
+                expanded={expanded === `panel${index}`}
+                onChange={handleChange(`panel${index}`)}
+              >
+                <AccordionSummary
+                  aria-controls={`panel${index}d-content`}
+                  id={`panel${index}d-header`}
+                >
+                  <Typography component="span">{item.questions}</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>{item.answer}</Typography>
+                </AccordionDetails>
+              </Accordion>
+            ))
+          ) : (
+            <p>Yükleniyor...</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Faq;
