bc3f69d82303a35a4bf39fe79f655c4cd4994f57
Umud <umudquliyev.1806@gmail.com>
2025-02-10 12:58:47 +0400

    fixed scrolltotop

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,20 @@ import Layout from "./layout/Layout";
 import Home from "./Home/Home";
 import "./App.css";
 import Details from "./components/Details/Details";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/telim/:id" element={<Details />} />
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/telim/:id" element={<Details />} />
+        </Route>
+      </Routes>
+
+      <ScrollToTop />
+    </>
   );
 }
 
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -15,7 +15,6 @@ const Home = () => {
       <Team />
       <FAQ />
       <Contact />
-      <ScrollToTop />
     </div>
   );
 };
