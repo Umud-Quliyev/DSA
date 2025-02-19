1459f2b50cedd6b4264181a2308f2ce7d3f0a489
Umud <umudquliyev.1806@gmail.com>
2025-02-19 11:59:20 +0400

    test

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -3,10 +3,6 @@ import "./Team.css";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/free-mode";
-import Box from "@mui/material/Box";
-import Modal from "@mui/material/Modal";
-import { Pagination } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
 import Teachers from "../Teachers/Teachers";
 import Graduates from "../Graduates/Graduates";
 
