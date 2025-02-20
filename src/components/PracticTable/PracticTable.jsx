d1834da5d355f7630faa41cc2ed32c881ce20fcc
Toghrul <adadsa>
2025-02-21 00:57:37 +0400

    usenavigate added

--- src/components/PracticTable/PracticTable.jsx
+++ src/components/PracticTable/PracticTable.jsx
@@ -1,6 +1,6 @@
 import { Skeleton } from "@mui/material";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const PracticTable = () => {
   const navigate = useNavigate();
